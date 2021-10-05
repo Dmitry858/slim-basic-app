ab13f80a46b6fa81490158f474daff1edff63b77
Dmitry <frontender@inbox.ru>
2021-10-05 12:39:24 +0400

    Add Blade template engine

--- app/routes.php
+++ app/routes.php
@@ -2,10 +2,25 @@
 use Slim\App;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use eftec\bladeone\BladeOne;
 
 return function(App $app) {
-    $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write("Hello world!");
+    function view(Response $response, $template, $with = []) {
+        $views = __DIR__ . '/../views';
+        $cache = __DIR__ . '/../cache';
+        $blade = new BladeOne($views, $cache,BladeOne::MODE_DEBUG);
+        $response->getBody()->write(
+            $blade->run($template, $with)
+        );
         return $response;
+    };
+
+    $app->get('/page', function (Request $request, Response $response) {
+        $title = 'Внутренняя страница';
+        return view($response, 'page', compact('title'));
+    });
+
+    $app->get('/', function (Request $request, Response $response) {
+        return view($response, 'home');
     });
 };
