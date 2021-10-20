4fab1b1184f4223f68abe6f788aec684ca82e439
Dmitry <frontender@inbox.ru>
2021-10-20 09:50:37 +0400

    Add illuminate/database

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 use DI\Container;
 use DI\Bridge\Slim\Bridge as SlimAppFactory;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 $app = SlimAppFactory::create(new Container);
 
@@ -9,5 +10,20 @@ $middleware($app);
 
 $routes = require __DIR__ . '/../app/routes.php';
 $routes($app);
+
+/**
+ * Initialize Illuminate Database Connection.
+ */
+$capsule = new Capsule;
+$capsule->addConnection([
+    'driver' => config('database.driver'),
+    'host' => config('database.host'),
+    'database' => config('database.database'),
+    'username' => config('database.username'),
+    'password' => config('database.password'),
+    'charset' => config('database.charset')
+]);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
 return $app;
---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => '3306',
+    'database' => 'slim-app',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8'
+];
