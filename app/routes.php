7d91514b99bc10060a1bec3ebcd218509585615c
Dmitry <frontender@inbox.ru>
2021-11-27 16:01:27 +0400

    Add routes for reset password

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,7 @@ use Slim\App;
 use App\Controllers\MainController;
 use App\Controllers\RegisterController;
 use App\Controllers\LoginController;
+use App\Controllers\ResetPasswordController;
 use App\Controllers\Api\ApiController;
 use App\Controllers\Api\ApiUserController;
 use App\Middleware\RedirectIfAuthMiddleware;
@@ -11,6 +12,8 @@ use Slim\Routing\RouteCollectorProxy;
 
 return function(App $app)
 {
+    $csrf = $app->getContainer()->get('csrf');
+
     $app->group('/api', function (RouteCollectorProxy $group) {
         $group->get('/users', [ApiUserController::class, 'getAll']);
         $group->get('/users/{id:[0-9]+}', [ApiUserController::class, 'get']);
@@ -22,7 +25,15 @@ return function(App $app)
         $group->post('register', [RegisterController::class, 'store']);
         $group->get('login', [LoginController::class, 'show']);
         $group->post('login', [LoginController::class, 'store']);
-    })->add(new RedirectIfAuthMiddleware())->add($app->getContainer()->get('csrf'));
+    })->add(new RedirectIfAuthMiddleware())->add($csrf);
+
+    $app->group('/', function (RouteCollectorProxy $group) {
+        $group->get('reset-password', [ResetPasswordController::class, 'send']);
+        $group->post('reset-password', [ResetPasswordController::class, 'store']);
+        $group->get('reset-password/confirm', [ResetPasswordController::class, 'confirm']);
+        $group->get('reset-password/{key}', [ResetPasswordController::class, 'show']);
+        $group->post('reset-password/{key}', [ResetPasswordController::class, 'update']);
+    })->add(new RedirectIfAuthMiddleware())->add($csrf);
 
     $app->get('/logout', [LoginController::class, 'logout'])->add(new RedirectIfGuestMiddleware());
 
