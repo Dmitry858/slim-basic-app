745ef2ad5f6634d46d1710fab589309e1edafafc
Dmitry <frontender@inbox.ru>
2021-11-29 12:11:47 +0400

    Add mail config

---
+++ config/mail.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'driver' => 'mail',
+    'host' => '',
+    'port' => '',
+    'from' => [
+        'address' => 'info@slim-app.loc',
+        'name' => 'Slim Basic App'
+    ],
+    'to' => [
+        'address' => '',
+        'name' => ''
+    ],
+    'smtp_auth' => false,
+    'username' => '',
+    'password' => ''
+];
