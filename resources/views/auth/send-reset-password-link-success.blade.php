c88fc1e2abb09365b6a0b84327d48a266af9c71b
Dmitry <frontender@inbox.ru>
2021-12-01 09:17:39 +0400

    Create send-reset-password-link-success view

---
+++ resources/views/auth/send-reset-password-link-success.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="content">
+        <h1>Восстановление пароля</h1>
+        <div class="form-signin">
+            <div class="alert alert-success" role="alert">
+                Ссылка на восстановление пароля отправлена на email. Проверьте почту и следуйте инструкции из письма.
+            </div>
+        </div>
+    </div>
+@endsection
