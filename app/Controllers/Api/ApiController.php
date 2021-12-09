54b39150e46994d6de19d346ecf07a19ea477918
Dmitry <frontender@inbox.ru>
2021-12-09 09:29:44 +0400

    Edit API controllers

--- app/Controllers/Api/ApiController.php
+++ app/Controllers/Api/ApiController.php
@@ -2,49 +2,14 @@
 
 namespace App\Controllers\Api;
 
-use App\Models\User;
-use Firebase\JWT\JWT;
+use Slim\App;
 
 class ApiController
 {
-    public function login($request, $response)
+    protected $cache;
+
+    public function __construct(App $app)
     {
-        $input = $request->getParsedBody();
-        $email = htmlspecialchars($input['email']);
-        $password = sha1($input['password']);
-
-        $user = User::where('email', $email);
-
-        if (!$user->exists())
-        {
-            $result['error'] = 'User not found';
-        }
-        else
-        {
-            $user = $user->first();
-
-            if ($password !== $user->password)
-            {
-                $result['error'] = 'The password is incorrect';
-            }
-            else
-            {
-                $key = config('api.token.secret_key');
-                $payload = array(
-                    'iat' => time(), // timestamp when the JWT was created
-                    'exp' => time() + config('api.token.expires_in'), // timestamp when JWT expires
-                    'data' => [
-                        'user_id' => $user->id,
-                        'email' => $user->email
-                    ]
-                );
-
-                $token = JWT::encode($payload, $key);
-                $result['result'] = ['token' => $token];
-            }
-        }
-
-        $response->getBody()->write(json_encode($result, JSON_PRETTY_PRINT));
-        return $response;
+        $this->cache = $app->getContainer()->get('cache');
     }
 }
--- app/Controllers/Api/ApiUserController.php
+++ app/Controllers/Api/ApiUserController.php
@@ -3,23 +3,80 @@
 namespace App\Controllers\Api;
 
 use App\Models\User;
+use Firebase\JWT\JWT;
 
-class ApiUserController
+class ApiUserController extends ApiController
 {
+    public function login($request, $response)
+    {
+        $input = $request->getParsedBody();
+        $email = htmlspecialchars($input['email']);
+        $password = sha1($input['password']);
+
+        $user = User::where('email', $email);
+
+        if (!$user->exists())
+        {
+            $result = [
+                'status' => 'error',
+                'message' => 'User not found'
+            ];
+        }
+        else
+        {
+            $user = $user->first();
+
+            if ($password !== $user->password)
+            {
+                $result = [
+                    'status' => 'error',
+                    'message' => 'The password is incorrect'
+                ];
+            }
+            else
+            {
+                $key = config('api.token.secret_key');
+                $payload = array(
+                    'iat' => time(), // timestamp when the JWT was created
+                    'exp' => time() + config('api.token.expires_in'), // timestamp when JWT expires
+                    'data' => [
+                        'user_id' => $user->id,
+                        'email' => $user->email
+                    ]
+                );
+
+                $token = JWT::encode($payload, $key);
+                $result = [
+                    'status' => 'success',
+                    'data' => $token
+                ];
+            }
+        }
+
+        $response->getBody()->write(json_encode($result));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
     public function getAll($response)
     {
         $users = User::all()->toArray();
-        $response->getBody()->write(json_encode($users, JSON_PRETTY_PRINT));
-        return $response;
+        $result = [
+            'status' => 'success',
+            'data' => $users
+        ];
+        $response->getBody()->write(json_encode($result));
+        return $response->withHeader('Content-Type', 'application/json');
     }
 
     public function get($response, $id)
     {
-        $result = [];
         $userId = intval($id);
         if ($userId === 0)
         {
-            $result['error'] = 'User not found';
+            $result = [
+                'status' => 'error',
+                'message' => 'User not found'
+            ];
         }
         else
         {
@@ -27,15 +84,21 @@ class ApiUserController
             if ($user)
             {
                 $user = $user->toArray();
-                $result['result'] = $user;
+                $result = [
+                    'status' => 'success',
+                    'data' => $user
+                ];
             }
             else
             {
-                $result['error'] = 'User not found';
+                $result = [
+                    'status' => 'error',
+                    'message' => 'User not found'
+                ];
             }
         }
 
-        $response->getBody()->write(json_encode($result, JSON_PRETTY_PRINT));
-        return $response;
+        $response->getBody()->write(json_encode($result));
+        return $response->withHeader('Content-Type', 'application/json');
     }
 }
