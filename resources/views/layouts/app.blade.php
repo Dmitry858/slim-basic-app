9506c9a7276c4aefae0efe93e0bac4a95b8ea440
Dmitry <frontender@inbox.ru>
2021-12-15 09:26:14 +0400

    Edit app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Тайтл страницы</title>
+    <title>{{ $title ?? 'Slim basic app' }}</title>
     <link rel="stylesheet" href="/css/bootstrap.css">
     <link rel="stylesheet" href="/css/app.css">
 </head>
