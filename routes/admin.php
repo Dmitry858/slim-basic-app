409a7ccfe00db09ef51fb0e3825c7ed58ff6352b
Dmitry <frontender@inbox.ru>
2021-12-29 10:40:08 +0400

    Edit admin routes

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use Slim\App;
 use App\Controllers\Admin\AdminController;
+use App\Controllers\Admin\UserController;
 use App\Middleware\RedirectIfNotAdminMiddleware;
 use Slim\Routing\RouteCollectorProxy;
 
@@ -9,5 +10,11 @@ return function(App $app)
 {
     $app->group(config('admin.path'), function (RouteCollectorProxy $group) {
         $group->get('/dashboard', [AdminController::class, 'index']);
+        $group->get('/users', [UserController::class, 'index']);
+        $group->get('/users/{id:[0-9]+}', [UserController::class, 'show']);
+        $group->post('/users/{id:[0-9]+}', [UserController::class, 'update']);
+        $group->get('/users/create', [UserController::class, 'create']);
+        $group->post('/users/create', [UserController::class, 'store']);
+        $group->delete('/users/{id:[0-9]+}', [UserController::class, 'delete']);
     })->add(new RedirectIfNotAdminMiddleware());
 };
