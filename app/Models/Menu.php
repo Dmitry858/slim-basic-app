8e102d850b618ef399523d7be81bda748290bb3e
Dmitry <frontender@inbox.ru>
2022-01-12 15:09:34 +0400

    Edit support class View

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -6,6 +6,8 @@ use \Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
+    protected $table = 'menu';
+
     protected $fillable = [
         'title',
         'link',
--- app/Support/View.php
+++ app/Support/View.php
@@ -5,6 +5,7 @@ namespace App\Support;
 use Slim\App;
 use eftec\bladeone\BladeOne;
 use Psr\Http\Message\ResponseInterface as Response;
+use App\Models\Menu;
 
 class View
 {
@@ -17,6 +18,11 @@ class View
 
     public function get(Response $response, $template, $with = []): Response
     {
+        if (strpos($_SERVER['REQUEST_URI'], config('admin.path')) === false)
+        {
+            $with['menu'] = $this->getMenuItems();
+        }
+
         $blade = new BladeOne(
             config('blade.views'),
             config('blade.cache'),
@@ -26,5 +32,27 @@ class View
             $blade->run($template, $with)
         );
         return $response;
+    }
+
+    private function getMenuItems(): array
+    {
+        if (config('app.cache.enable'))
+        {
+            if ($this->cache->get('menu'))
+            {
+                $menu = $this->cache->get('menu');
+            }
+            else
+            {
+                $menu = Menu::all()->toArray();
+                $this->cache->set('menu', $menu);
+            }
+        }
+        else
+        {
+            $menu = Menu::all()->toArray();
+        }
+
+        return $menu;
     }
 }
